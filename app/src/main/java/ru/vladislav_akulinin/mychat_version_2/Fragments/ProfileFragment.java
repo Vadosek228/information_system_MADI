cf3d2a5fd7cc1877b5bf03b089916b565c7b3229
Владислав Акулинин <akulininv@engne.ru>
2019-02-14 18:36:30 +0300

    Revert "Не работает при Glide при загрузке изоображения. Когда пользователь заходит в сообщение и затем нажимает "назад", то все крашится. Вызвано именно Glide, разобраться с жизненным циклом"
    This reverts commit a7001dd

--- app/src/main/java/ru/vladislav_akulinin/mychat_version_2/Fragments/ProfileFragment.java
+++ app/src/main/java/ru/vladislav_akulinin/mychat_version_2/Fragments/ProfileFragment.java
@@ -82,12 +82,12 @@ public class ProfileFragment extends Fragment {
 //                    Glide.with(getContext()).load(user.getImageURL()).into(image_profile);
 //                    Glide.with(ProfileFragment.this).load(user.getImageURL()).into(image_profile);
 
-                    //для обработки ошибки при загрузки изобржений
+//                    //для обработки ошибки при загрузки изобржений
 //                    RequestOptions ro = new RequestOptions();
 //                    ro.placeholder(R.drawable.ic_send);
 //                    ro.error(R.mipmap.ic_launcher);
 //
-                    //для загрузки изоображения
+//                    //для загрузки изоображения
 //                    Glide.with(ProfileFragment.this)
 //                            .applyDefaultRequestOptions(ro)
 //                            .load(user.getImageURL())
